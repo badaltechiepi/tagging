6c2049f9e7938e53daaaa7da27aed93eb6610dfd
badaltechiepi <badalkumarsahu92@gmail.com>
2020-10-06 14:49:33 +0530

    jenkinfile11

---
+++ git.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "# tagging" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/badaltechiepi/tagging.git
+git push -u origin main
